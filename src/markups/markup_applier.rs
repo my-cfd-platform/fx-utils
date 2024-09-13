e4f897b21712b27edbeb1d4dd814de1d25d05b47
RomanSavh <RomanSavh@users.noreply.github.com>
2024-09-13 13:10:36 +0300

    added min/max spread

--- src/markups/markup_applier.rs
+++ src/markups/markup_applier.rs
@@ -1,21 +1,201 @@
+use rust_decimal::prelude::ToPrimitive;
+use rust_decimal::{prelude::FromPrimitive, Decimal, RoundingStrategy};
+
 pub struct MarkupApplier {
     pub delta_bid: f64,
     pub delta_ask: f64,
+    pub max_spread: Option<f64>,
+    pub min_spread: Option<f64>,
+    pub digits: u32,
+    pub pip: f64,
+    pub factor: f64,
 }
 
-impl MarkupApplier {
-    pub fn create_empty() -> MarkupApplier {
+impl Default for MarkupApplier {
+    fn default() -> Self {
         MarkupApplier {
             delta_bid: 0.0,
             delta_ask: 0.0,
+            max_spread: None,
+            min_spread: None,
+            digits: 0,
+            pip: 0.0,
+            factor: 0.0,
         }
     }
+}
 
+impl MarkupApplier {
     pub fn apply_markup(&self, price: f64, is_bid: bool) -> f64 {
         if is_bid {
             price + self.delta_bid
         } else {
             price + self.delta_ask
         }
+    }
+
+    pub fn apply_min_max_spread(&self, price: f64, is_bid: bool) -> f64 {
+        if is_bid {
+            price + self.delta_bid
+        } else {
+            price + self.delta_ask
+        }
+    }
+}
+
+pub fn get_max_spread(
+    bid: f64,
+    ask: f64,
+    max_spread: f64,
+    asset_factor: f64,
+    pip: f64,
+    digits: u32,
+) -> Option<(f64, f64)> {
+    let bid = Decimal::from_f64(bid).unwrap();
+    let ask = Decimal::from_f64(ask).unwrap();
+    let pip = Decimal::from_f64(pip).unwrap();
+    let spread = ask - bid;
+    let max_spread = Decimal::from_f64(max_spread).unwrap();
+
+    if max_spread < spread {
+        let is_odd = (spread.round_dp(digits).to_f64().unwrap() * asset_factor) as i32 % 2 == 0;
+        let spread_diff = spread - max_spread;
+        let spread_rounded = (spread_diff / Decimal::from_f64(2.0).unwrap())
+            .round_dp_with_strategy(digits, RoundingStrategy::ToZero);
+
+        if is_odd {
+            return Some((
+                (bid + spread_rounded).to_f64().unwrap(),
+                (ask - spread_rounded).to_f64().unwrap(),
+            ));
+        } else {
+            return Some((
+                (bid + spread_rounded + pip).to_f64().unwrap(),
+                (ask - spread_rounded).to_f64().unwrap(),
+            ));
+        }
+    }
+
+    None
+}
+
+pub fn get_min_spread(
+    bid: f64,
+    ask: f64,
+    min_spread: f64,
+    asset_factor: f64,
+    pip: f64,
+    digits: u32,
+) -> Option<(f64, f64)> {
+    let bid = Decimal::from_f64(bid).unwrap();
+    let ask = Decimal::from_f64(ask).unwrap();
+    let pip = Decimal::from_f64(pip).unwrap();
+    let spread = ask - bid;
+    let min_spread = Decimal::from_f64(min_spread).unwrap();
+
+    if spread < min_spread {
+        let is_odd = (spread.round_dp(digits).to_f64().unwrap() * asset_factor) as i32 % 2 == 0;
+        let spread_diff = min_spread - spread;
+        let spread_rounded = (spread_diff / Decimal::from_f64(2.0).unwrap())
+            .round_dp_with_strategy(digits, RoundingStrategy::ToZero);
+
+        if is_odd {
+            return Some((
+                (bid - spread_rounded).to_f64().unwrap(),
+                (ask + spread_rounded).to_f64().unwrap(),
+            ));
+        } else {
+            return Some((
+                (bid - spread_rounded - pip).to_f64().unwrap(),
+                (ask + spread_rounded).to_f64().unwrap(),
+            ));
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_apply_max_spread() {
+        let (bid, ask) = get_max_spread(
+            1.23414,
+            1.23434,
+            0.00010,
+            10_f64.powi(5),
+            1.0 / 10_f64.powi(5),
+            5,
+        )
+        .unwrap();
+
+        assert_eq!(format!("{:.5}", bid), "1.23419");
+        assert_eq!(format!("{:.5}", ask), "1.23429");
+    }
+
+    #[test]
+    fn test_apply_max_spread2() {
+        let (bid, ask) = get_max_spread(
+            1.23414,
+            1.23434,
+            0.00010,
+            10_f64.powi(5),
+            1.0 / 10_f64.powi(5),
+            5,
+        )
+        .unwrap();
+
+        assert_eq!(format!("{:.5}", bid), "1.23419");
+        assert_eq!(format!("{:.5}", ask), "1.23429");
+    }
+
+    #[test]
+    fn test_apply_min_spread() {
+        let (bid, ask) = get_min_spread(
+            1.23434,
+            1.23435,
+            0.00010,
+            10_f64.powi(5),
+            1.0 / 10_f64.powi(5),
+            5,
+        )
+        .unwrap();
+
+        assert_eq!(format!("{:.5}", bid), "1.23429");
+        assert_eq!(format!("{:.5}", ask), "1.23439");
+    }
+
+    #[test]
+    fn test_apply_min_spread2() {
+        let (bid, ask) = get_min_spread(
+            1.23434,
+            1.23437,
+            0.00010,
+            10_f64.powi(5),
+            1.0 / 10_f64.powi(5),
+            5,
+        )
+        .unwrap();
+
+        assert_eq!(format!("{:.5}", bid), "1.23430");
+        assert_eq!(format!("{:.5}", ask), "1.23440");
+    }
+
+    #[test]
+    fn test_max_zero() {
+
+        let (bid, ask) = get_max_spread(
+            1.23434,
+            1.23436,
+            0.0,
+            10_f64.powi(5),
+            1.0 / 10_f64.powi(5),
+            5,
+        ).unwrap();
+
+        assert_eq!(format!("{:.5}", bid), "1.23435");
+        assert_eq!(format!("{:.5}", ask), "1.23435");
     }
 }
--- src/markups/markup_calculator.rs
+++ src/markups/markup_calculator.rs
@@ -28,7 +28,7 @@ pub trait IMarkupCalculator {
         let profile_id = self.get_markup_profile_id(group_id).await;
 
         if profile_id.is_none() {
-            return Ok(MarkupApplier::create_empty());
+            return Ok(MarkupApplier::default());
         }
 
         let profile_id = profile_id.unwrap();
@@ -36,35 +36,44 @@ pub trait IMarkupCalculator {
         let markup_profile = self.get_markup_profile(&profile_id).await;
 
         if markup_profile.is_none() {
-            return Ok(MarkupApplier::create_empty());
+            return Ok(MarkupApplier::default());
         }
 
         let markup_profile = markup_profile.unwrap();
 
         if markup_profile.disabled {
-            return Ok(MarkupApplier::create_empty());
+            return Ok(MarkupApplier::default());
         }
 
         let markup_by_instrument = markup_profile.instruments.get(instrument_id);
 
         if markup_by_instrument.is_none() {
-            return Ok(MarkupApplier::create_empty());
+            return Ok(MarkupApplier::default());
         }
 
         let markup_by_instrument = markup_by_instrument.unwrap();
 
-        let instrument_digits = self.get_instrument_digits(instrument_id).await;
+        let instrument_digits = self
+            .get_instrument_digits(instrument_id)
+            .await
+            .ok_or(MarkupCalculatorError::InstrumentNotFound)?;
 
-        if instrument_digits.is_none() {
-            return Err(MarkupCalculatorError::InstrumentNotFound);
-        }
-
-        let multiplier = 1.0 / i64::pow(10, instrument_digits.unwrap()) as f64;
+        let multiplier = 1.0 / i64::pow(10, instrument_digits) as f64;
+        let factor = i64::pow(10, instrument_digits) as f64;
+        let instrument_min_value = 1.0 / factor;
 
         Ok(MarkupApplier {
             delta_bid: multiplier * markup_by_instrument.markup_bid as f64,
             delta_ask: multiplier * markup_by_instrument.markup_ask as f64,
-        }
-        .into())
+            factor,
+            digits: instrument_digits,
+            max_spread: markup_by_instrument
+                .max_spread
+                .map(|x| (x as f64) * instrument_min_value),
+            min_spread: markup_by_instrument
+                .min_spread
+                .map(|x| (x as f64) * instrument_min_value),
+            pip: instrument_min_value,
+        })
     }
 }
